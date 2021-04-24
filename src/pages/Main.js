d36d9007c4aebee7ee24aed06aba292f8129a896
Gianpierre <fgianpierre@gmail.com>
2021-04-24 23:16:31 +0200

    add material-ui set-up and AppBar component

---
+++ src/pages/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+import "../App.css";
+
+const Main = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Main;
